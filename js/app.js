9b06a379a107aaa91f8bc9604f6af36c5a246edd
RolakeAnifowose <morolaanney@gmail.com>
2020-05-01 15:31:03 +0100

    Added the function for scrolling to section

--- js/app.js
+++ js/app.js
@@ -50,58 +50,30 @@ function createNavigationMenu() {
             let anchor = document.createElement("a");
             anchor.href = "";
             anchor.innerText = navbar__items[i];
-            // document.querySelector('#section2').scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
 
             let item = document.createElement("li");
 
             let itemValue = document.createTextNode(navbar__items[i]);
             
             item.appendChild(anchor);
-            // item.appendChild(itemValue);
             myList.appendChild(item);
-            console.log("hi")
         }
         container.appendChild(myList);
-        console.log("hi");
     }
 }
-
-function scrollToSection1() {
-    // this.slides.toArray()[this.index].nativeElement.scrollIntoView({behavior: 'smooth'});
-    let scrollItem = document.getElementById("section1");
-    scrollItem.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-}
-
-function scrollToSection2() {
-    let scrollItem = document.getElementById("section2");
-    scrollItem.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-}
-
-function scrollToSection3() {
-    let scrollItem = document.getElementById("section3");
-    scrollItem.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-}
-
-// let container = document.getElementsByClassName('navbar__menu');
-// var strs = [ "String 1", "String 2", "String 3" ];
-// var list = document.createElement("ul");
-// for (var i in strs) {
-//   var anchor = document.createElement("a");
-//   anchor.href = "#";
-//   anchor.innerText = strs[i];
-
-//   var elem = document.createElement("li");
-//   elem.appendChild(anchor);
-//   list.appendChild(elem);
-//   container.appendChild(list);
-//   console.log('Hi')
-// }
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToClick() {
+    let navbar = document.getElementById("navbar__list");
+    navbar.addEventListener('click', function (event) {
+        const clicked = document.querySelector('#' + event.target.dataset.nav)
+        clicked.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+       
+    }); console.log("Hey")
+};
 
 /**
  * End Main Functions
@@ -112,6 +84,13 @@ function scrollToSection3() {
 // Build menu 
 
 // Scroll to section on link click
+document.getElementById("navbar__list").addEventListener("click", function(e) {
+	if(e.target && e.target.nodeName == "LI") {
+        const click = document.querySelector('#' + event.target.dataset.nav)
+        click.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+		console.log("List item");
+    }
+});
 
 // Set sections as active
 
